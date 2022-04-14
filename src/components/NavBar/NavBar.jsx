6afb7b9f2a7a5ab5ade29373a3817b6538cad750
Alex Mellado <alex.mellado@aporta.com>
2022-04-14 14:04:30 -0400

    NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavBar.css';
+
+function NavBar(props) {
+    return (
+        <div>
+            <ul>
+                <a href="#">Opcion 1</a>
+                <a href="#">Opcion 2</a>
+                <a href="#">Opcion 3</a>
+            </ul>
+        </div>
+    )
+}
